957535181d36f26944d8392bb895cf22e21820b4
mah51 <mikiemike.hall@gmail.com>
2021-04-17 20:47:17 +0100

    actually fixing endpoint'

--- src/pages/spotify.tsx
+++ src/pages/spotify.tsx
@@ -40,8 +40,8 @@ function Spotify({ data, error }: SpotifyProps) {
 export async function getServerSideProps() {
   try {
     const { data } = await axios.get(
-      process.env.VERCEL
-        ? `${process.env.VERCEL_URL}/api/get-spotify-data`
+      process.env.NEXT_PUBLIC_VERCEL_ENV
+        ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/get-spotify-data`
         : `http://localhost:3000/api/get-spotify-data`,
     );
     return {
