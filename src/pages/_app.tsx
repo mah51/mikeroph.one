d9ffb26ac137c82c74ccf3ba86bb4707f5f0c46f
mah51 <mikiemike.hall@gmail.com>
2021-04-22 17:01:42 +0100

    more testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,57 +3,38 @@ import { ChakraProvider } from '@chakra-ui/react';
 import '@/styles/global.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
 import Loader from '@/Components/Loader';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import AppLayout from '../Components/AppLayout';
 import theme from '../../theme';
 
+Router.events.on(`routeChangeStart`, (url) => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+Router.events.on(`routeChangeComplete`, (url) => {
+  NProgress.done();
+  console.log(`done`);
+});
+Router.events.on(`routeChangeError`, () => NProgress.done());
+
 const queryClient = new QueryClient();
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
   useEffect(() => {
     document.documentElement.lang = `en-GB`;
-    const start = () => {
-      console.log(`start`);
-      setLoading(true);
-    };
-    const end = () => {
-      console.log(`findished`);
-      setLoading(false);
-    };
-    router.events.on(`routeChangeStart`, start);
-    router.events.on(`routeChangeComplete`, end);
-    router.events.on(`routeChangeError`, end);
-    return () => {
-      router.events.off(`routeChangeStart`, start);
-      router.events.off(`routeChangeComplete`, end);
-      router.events.off(`routeChangeError`, end);
-    };
   }, []);
 
   return (
     <>
-      {loading ? (
-        <h1
-          style={{
-            marginTop: `30vh`,
-            zIndex: 9999,
-            textAlign: `center`,
-            fontSize: `5em`,
-          }}
-        >
-          Loading!
-        </h1>
-      ) : (
-        <ChakraProvider theme={theme}>
-          <QueryClientProvider client={queryClient}>
-            <AppLayout>
-              <Component {...pageProps} />
-            </AppLayout>
-          </QueryClientProvider>
-        </ChakraProvider>
-      )}
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        </QueryClientProvider>
+      </ChakraProvider>
     </>
   );
 }
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -5,4 +5,11 @@ function Projects() {
   return <div>Test!</div>;
 }
 
+export async function getServerSideProps() {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  return { props: {} };
+}
+
 export default Projects;
