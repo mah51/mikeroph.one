208d88606d7c2d58948b87e64223da0529442dd9
mah51 <mikiemike.hall@gmail.com>
2021-04-23 03:10:06 +0100

    Fix: allow plausible.io security header

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.plausible.io;
   child-src *.youtube.com *.google.com *.twitter.com;
   style-src 'self' 'unsafe-inline' *.googleapis.com;
   img-src * blob: data:;
