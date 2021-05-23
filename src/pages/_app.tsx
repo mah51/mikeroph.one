299afb6ad91388f86dcbd0863f48340398d15138
mah51 <mikiemike.hall@gmail.com>
2021-05-23 18:01:06 +0100

    whoops

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -55,7 +55,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           selfHosted
           trackOutboundLinks
           enabled={process.env.NODE_ENV === 'production'}
-          customDomain={'http://stats.michael-hall.me/js/plausible.js'}
+          customDomain={'http://stats.michael-hall.me:8000'}
         >
           {loading ? (
             <Loader />
