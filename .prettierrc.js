651add1cafe6e0c75b6956fbe72884b35701a63c
mah51 <mah51@kent.ac.uk>
2021-06-19 01:26:35 +0100

    refactor(blogpostcard): re-design the blogpost card

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'css',
+}
--- components/BlogPost/BlogPost.tsx
+++ components/BlogPost/BlogPost.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 import {
   Link as ChakraLink,
   Text,
@@ -8,17 +8,18 @@ import {
   HStack,
   VStack,
   useBreakpointValue,
-} from '@chakra-ui/react';
-import formatDistance from 'date-fns/formatDistance';
-import { useQuery } from 'react-query';
-import BlogBadge from '../BlogBadge';
+  Flex,
+} from '@chakra-ui/react'
+import formatDistance from 'date-fns/formatDistance'
+import { useQuery } from 'react-query'
+import BlogBadge from '../BlogBadge'
 
 interface BlogPostProps {
-  title: string;
-  summary: string;
-  slug: string;
-  tags: string[];
-  publishedAt: string;
+  title: string
+  summary: string
+  slug: string
+  tags: string[]
+  publishedAt: string
 }
 
 export const BlogPost: React.FC<BlogPostProps> = ({
@@ -28,69 +29,59 @@ export const BlogPost: React.FC<BlogPostProps> = ({
   tags,
   publishedAt,
 }): React.ReactElement => {
-  const spliceBP = useBreakpointValue({ base: 2, md: 5 });
+  const spliceBP = useBreakpointValue({ base: 2, md: 4 })
   const { data } = useQuery(`views${slug}`, () => {
-    return fetch(`/api/views/${slug}`).then((res) => res.json());
-  });
-  const views = data?.total;
+    return fetch(`/api/views/${slug}`).then(res => res.json())
+  })
+  const views = data?.total
   return (
     <Link href={`/blog/${slug}`} passHref>
-      <ChakraLink _hover={{ textDecoration: `none` }} width="full">
-        <VStack
+      <ChakraLink _hover={{ textDecoration: `none` }} width='full'>
+        <Flex
           mb={8}
           px={5}
           py={4}
-          width="calc(100% -10px)"
+          direction='column'
+          width='calc(100% -10px)'
           mx={'5px'}
-          border="1px solid"
+          border='1px solid'
           bg={useColorModeValue(`white`, `gray.700`)}
           borderColor={useColorModeValue(`gray.200`, `gray.700`)}
-          boxShadow="lg"
-          transition="all 0.25s"
-          borderRadius="2xl"
-          transitionTimingFunction="spring(1 100 10 10)"
+          boxShadow='lg'
+          transition='all 0.25s'
+          borderRadius='2xl'
+          transitionTimingFunction='spring(1 100 10 10)'
           _hover={{ transform: `translateY(-4px)`, shadow: `xl` }}
         >
-          <HStack
-            width="full"
-            height="full"
-            alignItems="flex-end"
-            direction={{ base: `column`, md: `row` }}
-            justifyContent="space-between"
-            maxW="full"
-          >
-            <Heading fontSize={{ base: 'xl', md: '3xl' }} maxW="70%">
-              {title}
-            </Heading>
-            <Text fontWeight="semibold" fontSize="xl" pb={1}>
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`}
-            </Text>
-          </HStack>
-          <HStack
-            width="full"
+          <Flex
+            width='full'
             alignItems={{ base: 'flex-start', sm: 'flex-end' }}
-            flexDirection={{ base: `column-reverse`, sm: `row` }}
-            justifyContent="space-between"
-            maxW="full"
+            flexDirection={{ base: `column`, sm: `row` }}
+            justifyContent='space-between'
+            maxW='full'
+            mb={2}
           >
-            <Text mt={{ base: 2, sm: 0 }}>
-              Written{' '}
+            <HStack mb={{ base: 2, sm: 0 }} sx={{ marginLeft: '0px!important' }}>
+              {tags &&
+                tags.slice(0, spliceBP).map((tag, i) => <BlogBadge tag={tag} key={i.toString()} />)}
+            </HStack>
+            <Text
+              fontWeight='semibold'
+              fontSize='lg'
+              color={useColorModeValue('gray.600', 'gray.400')}
+            >
               {formatDistance(new Date(publishedAt), new Date(), {
                 addSuffix: true,
-              })}
+              })}{' '}
+              {'•'} {`${views ? new Number(views).toLocaleString() : '–––'} views`}
             </Text>
-            <HStack sx={{ marginLeft: '0px!important' }}>
-              {tags &&
-                tags
-                  .slice(0, spliceBP)
-                  .map((tag, i) => <BlogBadge tag={tag} key={i.toString()} />)}
-            </HStack>
-          </HStack>
-          <Text width="full" mt={2}>
-            {summary}
-          </Text>
-        </VStack>
+          </Flex>
+          <Heading mb={2} fontSize={{ base: '2xl', md: '3xl' }}>
+            {title}
+          </Heading>
+          <Text width='full'>{summary}</Text>
+        </Flex>
       </ChakraLink>
     </Link>
-  );
-};
+  )
+}
