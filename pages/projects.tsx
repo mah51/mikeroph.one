660832068344253b7577850926e02a1978c9b98a
mah51 <mah51@kent.ac.uk>
2021-06-22 20:13:08 +0100

    something is brewing

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,66 +1,38 @@
-import React from 'react';
-import { Box, Button, Flex, SimpleGrid, Text, VStack } from '@chakra-ui/react';
-import { FaGithub } from 'react-icons/fa';
-import { NextSeo } from 'next-seo';
-import LineHeading from '../components/LineHeading';
-import RepoCard from '../components/RepoCard';
-import PinnedProjects from '../components/PinnedProjects';
-import { pinnedRepos, pinnedRepoType } from '../data/pinnedRepos';
-import { repoType } from './api/github';
+import React from 'react'
+import { Box, Button, Flex, SimpleGrid, Text, VStack } from '@chakra-ui/react'
+import { FaGithub } from 'react-icons/fa'
+import { NextSeo } from 'next-seo'
+import LineHeading from '../components/LineHeading'
+import RepoCard from '../components/RepoCard'
+import PinnedProjects from '../components/PinnedProjects'
+import { pinnedRepos, pinnedRepoType } from '../data/pinnedRepos'
+import { repoType } from './api/github'
 
 interface ProjectsProps {
-  stars: number;
-  repos: repoType[];
-  followers: number;
-  revalidate?: number;
+  stars: number
+  repos: repoType[]
+  followers: number
+  revalidate?: number
 }
 
 function Projects({ repos }: ProjectsProps): React.ReactElement {
   return (
     <>
-      <NextSeo title="Projects" />
-      <Box
-        width="full"
-        px={3}
-        minH="100vh"
-        height="full"
-        mx="auto"
-        maxW="6xl"
-        py="28"
-      >
-        <Flex
-          direction="column"
-          alignItems="center"
-          width="full"
-          px={3}
-          height="full"
-          mx="auto"
-        >
-          <LineHeading
-            fontSize={{ base: `5xl`, md: `6xl` }}
-            mx="auto"
-            textAlign="center"
-          >
+      <NextSeo title='Projects' />
+      <Box width='full' px={3} minH='100vh' height='full' mx='auto' maxW='6xl' py='28'>
+        <Flex direction='column' alignItems='center' width='full' px={3} height='full' mx='auto'>
+          <LineHeading fontSize={{ base: `5xl`, md: `6xl` }} mx='auto' textAlign='center'>
             My projects
           </LineHeading>
           <Text mt={3}>A quick collection of my projects.</Text>
-          <VStack
-            direction="column"
-            my={16}
-            width="full"
-            height="full"
-            maxWidth="5xl"
-            spacing={10}
-          >
+          <VStack direction='column' my={16} width='full' height='full' maxWidth='5xl' spacing={10}>
             {pinnedRepos
               .sort(
                 (a: pinnedRepoType, b: pinnedRepoType) =>
                   new Date(
-                    repos.filter((x: repoType) => x.name === a.id)[0].created_at
+                    repos.filter((x: repoType) => x.name === a.id)[0]?.created_at
                   ).getTime() -
-                  new Date(
-                    repos.filter((y: repoType) => y.name === b.id)[0].created_at
-                  ).getTime()
+                  new Date(repos.filter((y: repoType) => y.name === b.id)[0]?.created_at).getTime()
               )
               .reverse()
               .map((data: pinnedRepoType, index) => (
@@ -72,18 +44,16 @@ function Projects({ repos }: ProjectsProps): React.ReactElement {
                 />
               ))}
           </VStack>
-          <LineHeading fontSize={{ base: `5xl`, lg: `5xl` }} textAlign="center">
+          <LineHeading fontSize={{ base: `5xl`, lg: `5xl` }} textAlign='center'>
             Repositories
           </LineHeading>
-          <Text mt={3}>
-            A list of all of the public repositories on my GitHub.
-          </Text>
+          <Text mt={3}>A list of all of the public repositories on my GitHub.</Text>
           <Button
-            as="a"
-            href="https://github.com/mah51"
-            variant="ghost"
-            colorScheme="brand"
-            size="lg"
+            as='a'
+            href='https://github.com/mah51'
+            variant='ghost'
+            colorScheme='brand'
+            size='lg'
             mt={5}
             leftIcon={<FaGithub />}
           >
@@ -95,21 +65,19 @@ function Projects({ repos }: ProjectsProps): React.ReactElement {
         <SimpleGrid
           mt={10}
           columns={{ base: 1, md: 2 }}
-          width="full"
-          height="full"
-          maxH="full"
-          mx="auto"
-          gridAutoRows="1fr"
+          width='full'
+          height='full'
+          maxH='full'
+          mx='auto'
+          gridAutoRows='1fr'
           spacingX={10}
           spacingY={8}
           isTruncated
-          overflow="visible"
+          overflow='visible'
         >
           {repos
             .sort(
-              (a: any, b: any) =>
-                new Date(a.pushed_at).getTime() -
-                new Date(b.pushed_at).getTime()
+              (a: any, b: any) => new Date(a.pushed_at).getTime() - new Date(b.pushed_at).getTime()
             )
             .reverse()
             .map((repo: repoType, index: number) => (
@@ -118,19 +86,16 @@ function Projects({ repos }: ProjectsProps): React.ReactElement {
         </SimpleGrid>
       </Box>
     </>
-  );
+  )
 }
 
 export async function getStaticProps(): Promise<{ props: ProjectsProps }> {
   const response = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_HOST ||
-      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    }/api/github`
-  );
+    `${process.env.NEXT_PUBLIC_HOST || `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`}/api/github`
+  )
 
-  const { stars, repos, followers } = await response.json();
-  return { props: { stars, repos, followers, revalidate: 600 } };
+  const { stars, repos, followers } = await response.json()
+  return { props: { stars, repos, followers, revalidate: 600 } }
 }
 
-export default Projects;
+export default Projects
