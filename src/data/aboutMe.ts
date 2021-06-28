885bee10b9008b7aa2d8f9b1d4db0542d9bebc53
mah51 <mah51@kent.ac.uk>
2021-06-28 03:03:59 +0100

    Start of new homepage

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,26 @@
+const info = [
+  {
+    input: 'self.getCurrentLocation()',
+    return: '"Kent, UK"',
+  },
+  {
+    input: 'self.contactMe',
+    return:
+      '["<a rel="noopener" href="https://www.linkedin.com/in/michael-hall-86616b17b/">LinkedIn</a>", "<a rel="noopener" href="https://github.com/mah51">github</a>", "<a rel="noopener" href="https://www.instagram.com/accounts/login/">instagram</a>"]',
+  },
+
+  {
+    input: 'self.interests',
+    return: '["web dev", "biology", "tennis"]',
+  },
+  {
+    input: 'self.education',
+    return: '"B.Sc Biochemistry - University of Kent"',
+  },
+  {
+    input: 'self.skills',
+    return: '[ "JavaScript", "Python", "React", "Next.JS", "Chakra-UI", "Tailwind", "SASS", "git"]',
+  },
+];
+
+export default info;
