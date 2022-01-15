3fdc5cdf992a82c92e2a1b96264b8205894cfe09
mah51 <mikiemike.hall@gmail.com>
2022-01-15 19:26:13 +0000

    adding contact info

---
+++ pages/api/contact.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import vCardsJS from 'vcards-js';
+
+const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+  if (req.query.id !== process.env.CONTACT_VCARD_ID) return res.send(404);
+  const vCard = vCardsJS();
+  vCard.firstName = 'Michael';
+  vCard.lastName = 'Hall';
+  vCard.email = 'michael.hall17@icloud.com';
+  vCard.birthday = new Date(2000, 5, 12);
+  vCard.photo.attachFromUrl(
+    'https://michael-hall.me/static/images/profile.jpeg',
+    'JPEG'
+  );
+  vCard.socialUrls['github'] = 'https://github.com/mah51';
+
+  res.setHeader('Content-Type', 'text/vcard; name=vcard.vcf');
+  res.setHeader('Content-Disposition', 'inline; filename=vcard.vcf');
+
+  res.send(vCard.getFormattedString());
+};
+
+export default handler;
